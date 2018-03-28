60e2ef200ecbb4d233096b66b45376d61ab21eaf
Tran Nam <admin@trans-macbook-air.local>
2018-03-28 15:24:29 +0700

    Initial Commit

---
+++ NoData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoData
+//
+//  Created by Tran Ngoc Nam on 3/28/18.
+//  Copyright © 2018 Tran Ngoc Nam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
